4208a4312302631eec1c13359e5a039592f7fa63
aak-ritie <aakritibhusal234@gmail.com>
2023-10-02 19:10:42 +0545

    funding

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,6 +5,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe{
 
     uint256 public minimumUSD=50;
@@ -12,14 +14,24 @@ contract FundMe{
     function fund() public payable 
      {
         //Want to be able to set a minimum fund amount in USD
-        require(msg.value >= minimumUSD,"Not enough fund");//1e18==1*10**18
+        require(getConversionRate(msg.value )>= minimumUSD,"Not enough fund");//1e18==1*10**18
 
     }
-    function getPrice() public{
+    function getPrice() public view returns(uint256){
         //ABI
         //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-         
+        AggregatorV3Interface priceFeed=AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int price,,,)=priceFeed.latestRoundData();
+        //ETH in terms of USD
+        return uint256(price * 1e10);
     }
+    function getConversionRate(uint ethAmount) public  view returns ( uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd= (ethPrice * ethAmount)/1e18;
+        return ethAmountInUsd;
+    }
+
 
 
 }
